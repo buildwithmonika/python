8367fba3c6c1a05f8de59a2a7e552173838a2cff
Monika <osdeveloper.monika@gmail.com>
2026-05-23 19:57:47 +0530

    Practice: beginner friendly number problems using python

---
+++ scripts/natural_numbers_sum.py
@@ -0,0 +1,21 @@
+#closest to n, divisible by m
+n = -15
+m = 6
+
+remainder = n % m
+print(n-remainder)
+
+
+# sum = 0
+# for i in range(1, 6):
+#     sum += i
+
+# print(sum)
+               
+# recurssion
+# def sum(n):
+#     if n == 1:
+#         return 1
+#     return n*n + sum(n-1)
+
+# print(sum(2))
